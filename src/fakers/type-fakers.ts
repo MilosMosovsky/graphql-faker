02f925102394ad28e0356a6c3a15cb8172cb18b8
Kristian Mandrup <kmandrup@gmail.com>
2018-11-17 19:13:25 +0100

    refactor to class TypeFakers

--- src/fakers/type-fakers.ts
+++ src/fakers/type-fakers.ts
@@ -1,63 +1,84 @@
+import { Base } from "../fake-schema/Base";
 const faker = require("faker");
 
-const defaults = {
-  Int: { min: 0, max: 99999 },
-  Float: { min: 0, max: 99999, precision: 0.01 },
-  String: {},
-  Boolean: {},
-  ID: { max: 9999999999, separator: ":" }
-};
+export const createTypeFakers = config => new TypeFakers(config).resolvers;
 
 // TODO: needs major rewrite/refactor!!!
-export function createTypeFakers(config: any = {}) {
-  const types = config.types || {};
-  const opts: any = {
-    ...defaults,
-    ...types
-  };
-  const customScalars =
-    typeof config.scalars === "function" ? config.scalars(opts) : {};
-  const primitives = {
-    Int: {
-      defaultOptions: opts.Int,
-      generator: options => {
-        options.precision = 1;
-        return () => faker.random.number(options);
-      }
-    },
-    Float: {
-      defaultOptions: opts.Float,
-      generator: options => {
-        return () => faker.random.number(options);
-      }
-    },
-    String: {
-      defaultOptions: opts.String,
-      generator: () => {
-        return () => "string";
-      }
-    },
-    Boolean: {
-      defaultOptions: opts.Boolean,
-      generator: () => {
-        return () => faker.random.boolean();
-      }
-    },
-    ID: {
-      defaultOptions: opts.ID,
-      generator: options => {
-        return parentType =>
-          new Buffer(
-            parentType.name +
-              options.separator +
-              faker.random.number(options).toString()
-          ).toString("base64");
-      }
-    }
-  };
+export class TypeFakers extends Base {
+  opts: any;
+  faker: any;
 
-  return {
-    ...primitives,
-    ...customScalars
-  };
+  constructor(config: any = {}) {
+    super(config);
+    const types = config.types || {};
+    this.faker = config.faker || faker;
+    this.opts = {
+      ...this.defaults,
+      ...types
+    };
+  }
+
+  get defaults() {
+    return {
+      Int: { min: 0, max: 99999 },
+      Float: { min: 0, max: 99999, precision: 0.01 },
+      String: {},
+      Boolean: {},
+      ID: { max: 9999999999, separator: ":" }
+    };
+  }
+
+  get primitives() {
+    const { opts, faker } = this;
+    return {
+      Int: {
+        defaultOptions: opts.Int,
+        generator: options => {
+          options.precision = 1;
+          return () => faker.random.number(options);
+        }
+      },
+      Float: {
+        defaultOptions: opts.Float,
+        generator: options => {
+          return () => faker.random.number(options);
+        }
+      },
+      String: {
+        defaultOptions: opts.String,
+        generator: () => {
+          return () => "string";
+        }
+      },
+      Boolean: {
+        defaultOptions: opts.Boolean,
+        generator: () => {
+          return () => faker.random.boolean();
+        }
+      },
+      ID: {
+        defaultOptions: opts.ID,
+        generator: options => {
+          return parentType =>
+            new Buffer(
+              parentType.name +
+                options.separator +
+                faker.random.number(options).toString()
+            ).toString("base64");
+        }
+      }
+    };
+  }
+
+  get customScalars() {
+    const { opts, config } = this;
+    return typeof config.scalars === "function" ? config.scalars(opts) : {};
+  }
+
+  get resolvers() {
+    return {
+      ...this.primitives,
+      ...this.customScalars
+    };
+  }
 }
